df86ff890ee776b26e4964288e7291d3de052848
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2020-03-26 14:23:12 +0200

    Fresh start

--- src/main/java/fi/vayla/yksityistie/model/ReportType.java
+++
@@ -1,7 +0,0 @@
-package fi.vayla.yksityistie.model;
-
-public enum ReportType {
-	REPORT_CHANGES,
-	CORRECT,
-	NO_RESTRICTIONS
-}
--- src/main/java/fi/vayla/yksityistie/model/VoucherDeliveryMethod.java
+++
@@ -1,7 +0,0 @@
-package fi.vayla.yksityistie.model;
-
-public enum VoucherDeliveryMethod {
-	EMAIL,
-	PDF,
-	EMAIL_AND_PDF
-}
