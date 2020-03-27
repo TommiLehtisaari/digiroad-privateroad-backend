c54bcc747e31e38481c3c8fe15bfa89acd341f62
Tommi Lehtisaari <tommi@Tommi-MacBook-Pro.local>
2020-03-27 12:00:42 +0200

    Configure eslint and prettier with typescript

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,44 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express'
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
-const app: Application = express();
+const app: Application = express()
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!');
-});
+  res.send('Hello world!')
+})
 
 const server = app.listen(PORT, () => {
-  console.log(`Express Running on port: ${PORT} 🚀`);
-});
-
-// quit on ctrl-c when running docker in terminal
-process.on('SIGINT', function onSigint() {
-  console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
-  shutdown();
-});
-
-// quit properly on docker stop
-process.on('SIGTERM', function onSigterm() {
-  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
-  shutdown();
-});
+  console.log(`Express Running on port: ${PORT} 🚀`)
+})
 
 // shut down server
-function shutdown() {
+const shutdown = (): void => {
   // NOTE: server.close is for express based apps
   // If using hapi, use `server.stop`
-  server.close(function onServerClosed(err) {
+  server.close((err) => {
     if (err) {
-      console.error(err);
-      process.exitCode = 1;
+      console.error(err)
+      process.exitCode = 1
     }
-    process.exit();
-  });
+    process.exit()
+  })
 }
+
+// quit on ctrl-c when running docker in terminal
+process.on('SIGINT', () => {
+  console.info(
+    'Got SIGINT (aka ctrl-c in docker). Graceful shutdown ',
+    new Date().toISOString()
+  )
+  shutdown()
+})
+
+// quit properly on docker stop
+process.on('SIGTERM', () => {
+  console.info(
+    'Got SIGTERM (docker container stop). Graceful shutdown ',
+    new Date().toISOString()
+  )
+  shutdown()
+})
